00231a1d62cfd7c157cae0ea082f5404b5f13abd
karan jariwala <karan.j@practo.com>
2018-03-24 17:22:53 +0530

    adding responsiveness on summary page

--- src/questionsApp/Components/presentational/SummaryCard.jsx
+++ src/questionsApp/Components/presentational/SummaryCard.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import { StyledQuestion } from './StyledComponenetsCommon';
+import '../../App.css';
 
 const StyledCard = styled.div`
   margin: 0 auto;
   text-align: left;
-  width: 40%;
+  width: 35%;
 `;
 
 const StyledAnswer = styled.h4`
